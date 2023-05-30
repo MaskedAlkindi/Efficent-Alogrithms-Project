baa2cc18a7673fad0ce919efcbd6e77fee76f8b6
salim439 <105734161+MaskedAlkindi@users.noreply.github.com>
2023-05-30 23:22:33 +0400

    Last Commit

--- Front-End/React/src/components/NearestLocationChoice.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from "react";
-import styles from "./NearestLocationChoice.module.css";
-const NearestLocationChoice: FunctionComponent = () => {
-  return <div className={styles.nearestChoice}>Nearest [Choice]</div>;
-};
-
-export default NearestLocationChoice;
